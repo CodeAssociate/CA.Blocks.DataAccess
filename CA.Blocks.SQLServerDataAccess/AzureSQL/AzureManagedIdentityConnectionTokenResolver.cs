172672c78f6f7f8e3f66afeb252ff7302d745c2d
KevinBosch <kevin.bosch@codeassociate.com>
2021-11-21 16:41:40 +1300

    re 28

---
+++ CA.Blocks.SQLServerDataAccess/AzureSQL/AzureManagedIdentityConnectionTokenResolver.cs
@@ -0,0 +1,41 @@
+﻿using Azure.Core;
+
+namespace CA.Blocks.SQLServerDataAccess.AzureSQL
+{
+    public class AzureManagedIdentityConnectionTokenResolver : IConnectionTokenResolver
+    {
+        private readonly TokenCredential _tokenCredential;
+
+        public AzureManagedIdentityConnectionTokenResolver(TokenCredential tokenCredential)
+        {
+            _tokenCredential = tokenCredential;
+        }
+
+        private string _existingToken;
+        /// <summary>
+        /// Get a new Azure Token
+        /// </summary>
+        /// <returns></returns>
+        public string GetConnectionToken(string connectionString)
+        {
+            if (_existingToken != null)
+            {
+                return _existingToken;
+            }
+
+            if (connectionString.Contains("database.windows.net") && !(connectionString.Contains("Password=")))
+            {
+                var token = _tokenCredential?.GetToken(
+                    new TokenRequestContext(new[] { "https://database.windows.net" }),
+                    default);
+                _existingToken = token?.Token;
+                return _existingToken;
+            }
+            else
+            {
+                // The connection string for a AzureManagedIdentityC must not contain a password and must be for a database in the realm  database.windows.net
+                return null;
+            }
+        }
+    }
+}
---
+++ CA.Blocks.SQLServerDataAccess/AzureSQL/AzureSqlServerDataAccess.cs
@@ -0,0 +1,27 @@
+﻿using CA.Blocks.DataAccess.DI;
+using CA.Blocks.DataAccess.Translator.DbRowToObject.Interfaces;
+
+namespace CA.Blocks.SQLServerDataAccess.AzureSQL
+{
+    /// <summary>
+    /// This class provides a wrapper for Managed Identity authentication using tokens.  This is a common pattern used in Azure where you can authenticate using a Token. Other than that this is  just like the SqlServerDataAccess class 
+    /// </summary>
+    public class AzureSqlServerDataAccess : SqlServerDataAccess
+    {
+        private readonly IConnectionTokenResolver _connectionTokenResolver;
+
+        public AzureSqlServerDataAccess(IDataAccessConfig config, IConnectionTokenResolver connectionTokenResolver, IDbRowTranslatorProvider dbRowTranslatorProvider = null) : base(config, dbRowTranslatorProvider)
+        {
+            _connectionTokenResolver = connectionTokenResolver;
+        }
+
+        /// <summary>
+        /// Get the Token from the registered  IConnectionTokenResolver provider
+        /// </summary>
+        /// <returns></returns>
+        protected override string GetConnectionToken()
+        {
+            return _connectionTokenResolver?.GetConnectionToken(ConnectionString);
+        }
+    }
+}
