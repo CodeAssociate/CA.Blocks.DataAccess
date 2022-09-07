928e1f78b48851fe93d5f1823145560544d4b4e7
Kevin Bosch <8487763+KevinBosch@users.noreply.github.com>
2022-09-08 08:30:52 +1200

    Add The Ability to Specify A Converter name for more option on auto convert

---
+++ CA.Blocks.DataAccess/Translator/DbColToType/AttributeExtensions/DbColToTypeConverterAttribute.cs
@@ -0,0 +1,33 @@
+﻿using CA.Blocks.DataAccess.Translator.DbColToType.Interfaces;
+using System;
+
+namespace CA.Blocks.DataAccess.Translator.DbColToType.AttributeExtensions
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class DbColToTypeConverterAttribute : Attribute
+    {
+
+        private readonly Type _converterType;
+
+        public Type ConverterType => this._converterType;
+
+        public object[] ConverterParameters { get; private set; }
+
+
+        public DbColToTypeConverterAttribute(Type converterType)
+        {
+            this._converterType = (object)converterType != null ? converterType : throw new ArgumentNullException(nameof(converterType));
+            var hasInterface = converterType.GetInterface(typeof(IDbColToTypeConverter).FullName);
+            if (hasInterface == null)
+            {
+                throw new ArgumentException("The Converter must be of type IDbColToTypeConverter", nameof(converterType));
+            }
+        }
+
+        public DbColToTypeConverterAttribute(Type converterType, params object[] converterParameters)
+            : this(converterType)
+        {
+            this.ConverterParameters = converterParameters;
+        }
+    }
+}
