5a8412f0a3daa458f3e9bc0976cf0cce0e101ba0
kevin <kevin@ravin.co.nz>
2025-11-27 15:35:43 +1300

    Add Support to return nested object as readonly list from Json using the  JsonDbInterfacedColToTypeConverter

---
+++ CA.Blocks.DataAccess.Extensions.Translators.Json/Converters/JsonDbInterfacedColToTypeConverter.cs
@@ -0,0 +1,48 @@
+﻿using System.Data;
+using System.Text.Json;
+using CA.Blocks.DataAccess.Translator.DbColToType.Converters;
+
+namespace CA.Blocks.DataAccess.Extensions.Translators.Json.Converters
+{
+    public class JsonDbInterfacedColToTypeConverter<I, T> : BaseDbColToTypeConverter<I>
+           where T : I, new()
+    {
+        private readonly JsonSerializerOptions _options;
+        public JsonDbInterfacedColToTypeConverter(JsonSerializerOptions options)
+        {
+            _options = options;
+        }
+
+        public JsonDbInterfacedColToTypeConverter()
+        {
+            _options = null;
+        }
+
+        private I ToObject(string json)
+        {
+            return string.IsNullOrEmpty(json) ?
+                new T() :
+                JsonSerializer.Deserialize<T>(json, _options);
+        }
+
+        public override I GetDataValue(DataRow dr, string columnName)
+        {
+            return ToObject(dr.AsString(columnName));
+        }
+
+        public override I GetDataValue(IDataReader dr, string columnName)
+        {
+            return ToObject(dr.AsString(columnName));
+        }
+
+        public override I GetDataValue(DataRow dr, int columnIndex)
+        {
+            return ToObject(dr.AsString(columnIndex));
+        }
+
+        public override I GetDataValue(IDataReader dr, int columnIndex)
+        {
+            return ToObject(dr.AsString(columnIndex));
+        }
+    }
+}
--- CA.Blocks.DataAccess.Extensions.Translators.JsonTests/Converters/JsonDbColToTypeConverterUnitTests.cs
+++ CA.Blocks.DataAccess.Extensions.Translators.JsonTests/Converters/JsonDbColToTypeConverterUnitTests.cs
@@ -1,6 +1,6 @@
-﻿using System.Text.Json;
-using CA.Blocks.DataAccess.Extensions.Translators.Json.Converters;
+﻿using CA.Blocks.DataAccess.Extensions.Translators.Json.Converters;
 using CA.Blocks.DataAccessUnitTests.Translator.DbColToType.Converters;
+using System.Text.Json;
 
 namespace CA.Blocks.DataAccess.Extensions.Translators.JsonTests.Converters
 {
@@ -11,6 +11,7 @@ namespace CA.Blocks.DataAccess.Extensions.Translators.JsonTests.Converters
 
         public static string TestData =
             @"[{""colour"": ""red"",""value"": ""#f00""},{""colour"": ""green"",""value"": ""#0f0""}]";
+
 
         private class ColourValueDataType
         {
@@ -75,6 +76,85 @@ namespace CA.Blocks.DataAccess.Extensions.Translators.JsonTests.Converters
             Assert.That(target.GetDataValue(dataReader, 1)![1].Colour, Is.EqualTo("green"));
 
         }
-        
+
+
+        [Test]
+        public void DbColToTypeReadOnlyConverterTest()
+        {
+            var dt = CreateTestTable(typeof(string), TestData);
+            var dataRow = GetDataRow(1, dt);
+            var dataReader = GetDataReader(1, dt);
+
+            //var expected = new List<ColourValueDataType>
+            //{
+            //    new ColourValueDataType { Colour = "red", Value = "#f00" },
+            //    new ColourValueDataType { Colour = "green", Value = "#0f0" }
+            //};
+
+            var target = new JsonDbInterfacedColToTypeConverter<
+                    IReadOnlyList<ColourValueDataType>, List<ColourValueDataType>>
+                    (new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+            var obj = target.GetDataValue(dataRow, 1);
+            Assert.That(obj, Is.InstanceOf<IReadOnlyList<ColourValueDataType>>()); 
+
+
+            Assert.That(target.GetDataValue(dataRow, "col")[1].Colour, Is.EqualTo("green"));
+            Assert.That(target.GetDataValue(dataRow, 1)[1].Colour, Is.EqualTo("green"));
+
+
+            Assert.That(target.GetDataValue(dataReader, "col")[1].Colour, Is.EqualTo("green"));
+            Assert.That(target.GetDataValue(dataReader, 1)[1].Colour, Is.EqualTo("green"));
+        }
+
+
+        [Test]
+        public void DbColToTypeEmptyReadOnlyConverterTest()
+        {
+            var dt = CreateTestTable(typeof(string), "");
+            var dataRow = GetDataRow(1, dt);
+            var dataReader = GetDataReader(1, dt);
+
+
+            var target = new JsonDbInterfacedColToTypeConverter<
+                    IReadOnlyList<ColourValueDataType>, List<ColourValueDataType>>
+                    (new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+            var obj = target.GetDataValue(dataRow, 1);
+            Assert.That(obj, Is.InstanceOf<IReadOnlyList<ColourValueDataType>>());
+        }
+
+        [Test]
+        public void DbColToTypeEmptyNullReadOnlyConverterTest()
+        {
+            var dt = CreateTestTable(typeof(string), null);
+            var dataRow = GetDataRow(1, dt);
+            var dataReader = GetDataReader(1, dt);
+
+
+            var target = new JsonDbInterfacedColToTypeConverter<
+                    IReadOnlyList<ColourValueDataType>, List<ColourValueDataType>>
+                    (new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+            var obj = target.GetDataValue(dataRow, 1);
+            Assert.That(obj, Is.InstanceOf<IReadOnlyList<ColourValueDataType>>());
+        }
+
+        [Test]
+        public void DbColToTypeEmptyArrayReadOnlyConverterTest()
+        {
+            var dt = CreateTestTable(typeof(string), "[]");
+            var dataRow = GetDataRow(1, dt);
+            var dataReader = GetDataReader(1, dt);
+
+
+            var target = new JsonDbInterfacedColToTypeConverter<
+                    IReadOnlyList<ColourValueDataType>, List<ColourValueDataType>>
+                    (new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+            var obj = target.GetDataValue(dataRow, 1);
+            Assert.That(obj, Is.InstanceOf<IReadOnlyList<ColourValueDataType>>());
+        }
+
     }
 }
