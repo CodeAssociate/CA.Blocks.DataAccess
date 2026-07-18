96dd43f73475865b2606c100486743f757e2c8a1
kevin <8487763+KevinBosch@users.noreply.github.com>
2026-07-19 09:18:38 +1200

    Add files

---
+++ CA.Blocks.SQLServer.AppHost/ConfigSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace CA.Blocks.SQLServer.AppHost;
+
+public class ConfigSettings
+{
+    public const string SERVER_SERVICE_NAME = "sqlServer";
+    public const string DB_NAME = "ca-blocks-db";
+}
